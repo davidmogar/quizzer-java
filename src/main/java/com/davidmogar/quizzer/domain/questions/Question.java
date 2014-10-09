e7eaa2b8902c38d0c01b8653a41afa63e7505cef
David Moreno García <david.mogar@gmail.com>
2014-10-09 16:26:23 +0200

    Update Question model

--- src/main/java/com/davidmogar/quizzer/domain/questions/Question.java
+++ src/main/java/com/davidmogar/quizzer/domain/questions/Question.java
@@ -1,7 +1,29 @@
 package com.davidmogar.quizzer.domain.questions;
 
-/**
- * Created by David on 10/9/2014.
- */
-public class Question {
+import com.davidmogar.quizzer.domain.Answer;
+
+public abstract class Question {
+
+    protected String questionText;
+    protected long id;
+
+    public abstract double getScore(Answer answer);
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+
 }
