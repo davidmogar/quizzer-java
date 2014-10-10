202858c15ac3e8b3e651813a3d81d596d0dae032
David Moreno García <david.mogar@gmail.com>
2014-10-10 18:33:30 +0200

    Add Freemark template and an index page

--- src/main/java/com/davidmogar/quizzer/Server.java
+++ src/main/java/com/davidmogar/quizzer/Server.java
@@ -3,44 +3,46 @@ package com.davidmogar.quizzer;
 import com.davidmogar.quizzer.loaders.AssessmentLoader;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import spark.Request;
-import spark.Response;
-import spark.Route;
+import spark.ModelAndView;
+import spark.template.freemarker.*;
 
 import java.io.IOException;
-import java.util.Set;
+import java.util.HashMap;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.SparkBase.setPort;
+import static spark.SparkBase.staticFileLocation;
 
 public class Server {
 
     public static void startServer() {
+        setPort(3000);
+
+        /* Static files location */
+        staticFileLocation("public");
+
+        /* Get index page */
         get("/", (req, res) -> {
-            return "<html><body><form action=\"/\" method=\"POST\"><textarea name=\"questions\" rows=\"10\" " +
-                    "cols=\"50\"></textarea><textarea name=\"answers\" rows=\"10\" cols=\"50\"></textarea><input " +
-                    "type=\"submit\" " +
-                    "value=\"Generate " +
-                    "grades\"/></form></body></html>";
-        });
+            return new ModelAndView(new HashMap<String, Object>(), "index.ftl");
+        }, new FreeMarkerEngine());
 
-        createPostRoutes();
-    }
-
-    private static void createPostRoutes() {
+        /* Post questions and answers */
         post("/", (req, res) -> {
-            Set<String> params = req.queryParams();
             String questionsJson = req.queryParams("questions");
             String answersJson = req.queryParams("answers");
 
-            String response;
+            String response = "Invalid input";
 
-            try {
-                Assessment assessment = AssessmentLoader.loadAssessment(questionsJson, answersJson, null);
-                assessment.calculateGrades();
-                Gson gson = new GsonBuilder().create();
-                response = gson.toJson(assessment.getGrades());
-            } catch (IOException e) {
-                response = "Invalid input";
+            if (questionsJson != null && answersJson != null) {
+                try {
+                    Assessment assessment = AssessmentLoader.loadAssessment(questionsJson, answersJson, null);
+                    assessment.calculateGrades();
+                    Gson gson = new GsonBuilder().create();
+                    response = gson.toJson(assessment.getGrades());
+                } catch (IOException e) {
+                    /* Return default value */
+                }
             }
 
             return response;
