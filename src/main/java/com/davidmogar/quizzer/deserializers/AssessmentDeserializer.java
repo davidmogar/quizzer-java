6862d039fb1b1cb41fe3063afc750ad934ef6f8b
David Moreno García <david.mogar@gmail.com>
2014-10-10 09:13:29 +0200

    Refactor AssessmentDeserializer

--- src/main/java/com/davidmogar/quizzer/deserializers/AssessmentDeserializer.java
+++ src/main/java/com/davidmogar/quizzer/deserializers/AssessmentDeserializer.java
@@ -3,52 +3,37 @@ package com.davidmogar.quizzer.deserializers;
 import com.davidmogar.quizzer.domain.Answer;
 import com.davidmogar.quizzer.domain.Grade;
 import com.davidmogar.quizzer.domain.questions.Question;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
+import com.google.gson.*;
 
-import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
 
 public class AssessmentDeserializer {
 
+    @SuppressWarnings("unchecked")
     public static HashMap<Long, List<Answer>> deserializeAnswers(String json) {
-        JsonParser parser = new JsonParser();
-        JsonObject rootObject = parser.parse(json).getAsJsonObject();
-
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.registerTypeAdapter(HashMap.class, new AnswersDeserializer());
-        Gson gson = gsonBuilder.create();
-        Type type = new TypeToken<HashMap<Long, Answer>>(){}.getType();
-
-        return gson.fromJson(rootObject, type);
+        return deserialize(json, new AnswersDeserializer());
     }
 
+    @SuppressWarnings("unchecked")
     public static HashMap<Long, Grade> deserializeGrades(String json) {
-        JsonParser parser = new JsonParser();
-        JsonObject rootObject = parser.parse(json).getAsJsonObject();
-
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.registerTypeAdapter(HashMap.class, new GradesDeserializer());
-        Gson gson = gsonBuilder.create();
-        Type type = new TypeToken<HashMap<Long, Grade>>(){}.getType();
-
-        return gson.fromJson(rootObject, type);
+        return deserialize(json, new GradesDeserializer());
     }
 
+    @SuppressWarnings("unchecked")
     public static HashMap<Long, Question> deserializeQuestions(String json) {
+        return deserialize(json, new QuestionsDeserializer());
+    }
+
+    public static HashMap deserialize(String json, JsonDeserializer deserializer) {
         JsonParser parser = new JsonParser();
         JsonObject rootObject = parser.parse(json).getAsJsonObject();
 
         GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.registerTypeAdapter(HashMap.class, new QuestionsDeserializer());
+        gsonBuilder.registerTypeAdapter(HashMap.class, deserializer);
         Gson gson = gsonBuilder.create();
-        Type type = new TypeToken<HashMap<Long, Question>>(){}.getType();
 
-        return gson.fromJson(rootObject, type);
+        return gson.fromJson(rootObject, HashMap.class);
     }
 
 }
